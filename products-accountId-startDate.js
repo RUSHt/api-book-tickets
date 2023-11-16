41312d3b504233c4a90c0bc77e311fcc4fba70e8
RUSHt <petera@rush.technology>
2023-11-16 07:59:48 +0000

    added gitPage

--- products-accountId-startDate.js
+++ products-accountId-startDate.js
@@ -1,3 +1,5 @@
 export const products  = [ { _id: 'a', title: "Curly Fries", price: 3.75 }, { _id: 'b', title: "Meat Platter", price: 9.75 }, { _id: 'c', title: "Salted Popcorn", price: 3.95 }, { _id: 'd', title: "Chocolate Buttons", price: 3.5 } ]
 export const accountId = '02sxk0vxanh'
-export const startDate = '2023/06/01'
+export const startDate = '2023/06/01';
+
+// added gitPages
